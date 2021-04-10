d9ed1a09d8f86b306e963bd342d538f6b4184aa2
Yann PELUD <ypelud@gmail.com>
2021-04-10 17:51:20 +0200

    add tools certs

---
+++ webhook-patch-ca-bundle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+CA_BUNDLE=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}')
+
+if [ -z "${CA_BUNDLE}" ]; then
+    CA_BUNDLE=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.ca\.crt}")
+fi
+
+sed -i "" -e "s/caBundle:.*/caBundle: ${CA_BUNDLE}/g" k8s/mutating.yaml
